edefad3315dba9c34ed96c4a9ccb0256f015f64c
Alnavv9 <89711034+Alnavv9@users.noreply.github.com>
2023-06-29 23:06:00 -0400

    actualizado farmacia.py

--- farmacia.py
+++ farmacia.py
@@ -1,6 +1,5 @@
 import requests
 import csv
-from google.cloud import storage
 
 # Obtener los datos de la API
 api_url = "https://midas.minsal.cl/farmacia_v2/WS/getLocalesTurnos.php"
@@ -13,21 +12,19 @@ csv_file = "locales_turnos.csv"
 # Guardar los datos en un archivo CSV
 with open(csv_file, 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
-    writer.writerow(["Fecha","ID", "Nombre local", "Comuna", "Direccion", "apertura","cierre",
-    "telefono","dia funcionamiento"])  # Especifica las columnas deseadas
-    
+    writer.writerow(["Fecha", "ID", "Nombre local", "Comuna", "Dirección", "Apertura", "Cierre", "Teléfono", "Día funcionamiento"])  # Especifica las columnas deseadas
+
     for item in data:
-        writer.writerow([item["fecha"],item["local_id"], item["local_nombre"], item["comuna_nombre"], item["local_direccion"], 
-        item["funcionamiento_hora_apertura"],item["funcionamiento_hora_cierre"],item["local_telefono"],
-        item["funcionamiento_dia"]])
+        writer.writerow([
+            item["fecha"],
+            item["local_id"],
+            item["local_nombre"],
+            item["comuna_nombre"],
+            item["local_direccion"],
+            item["funcionamiento_hora_apertura"],
+            item["funcionamiento_hora_cierre"],
+            item["local_telefono"],
+            item["funcionamiento_dia"]
+        ])
 
-# Cargar el archivo CSV en Google Cloud Storage
-bucket_name = "dataflow-apache-quickstart_evaluacion-3-390120"  # Reemplaza con el nombre de tu bucket en GCP
-destination_blob_name = "locales_turnos.csv"  # Nombre y ruta del archivo en GCP
-
-storage_client = storage.Client()
-bucket = storage_client.get_bucket(bucket_name)
-blob = bucket.blob(destination_blob_name)
-blob.upload_from_filename(csv_file)
-
-print("Archivo cargado exitosamente en Google Cloud Storage:", blob.name)
+print("Datos guardados exitosamente en el archivo CSV:", csv_file)
